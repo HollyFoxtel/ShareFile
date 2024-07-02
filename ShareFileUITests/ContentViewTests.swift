35f2a38bcdd1527b9c13ff6bf4459bebb17f71b8
Holly <hao.li@foxtel.com.au>
2024-07-02 10:09:30 +0800

    [UITest] refactor the content view test

--- ShareFileUITests/ContentViewTests.swift
+++ ShareFileUITests/ContentViewTests.swift
@@ -1,19 +1,42 @@
 import XCTest
 
+private var isSwiftTestingEnable: Bool = {
+    var enabled = false
+    #if canImport(Testing)
+    enabled = true
+    #endif
+    return enabled
+}()
+
+struct ContentPage {
+    let app: XCUIApplication
+
+    init(app: XCUIApplication) {
+        self.app = app
+    }
+
+    var swiftTestingLabel: XCUIElement {
+        app.staticTexts["Hello Swift Testing"]
+    }
+}
+
 class ContentViewUITests: XCTestCase {
     private var app: XCUIApplication!
-    
+    private var page: ContentPage!
+
     override func setUpWithError() throws {
+        print("[init] SwiftTesting Enable Status: \(isSwiftTestingEnable)")
+
         continueAfterFailure = false
         app = XCUIApplication()
+        page = ContentPage(app: app)
         app.launch()
     }
 
     override func tearDownWithError() throws {
     }
 
-    func testShowLabelTextDisplay() throws {
-        let helloWorldText = app.staticTexts["Hello, world!"]
-        XCTAssertTrue(helloWorldText.exists, "The 'Hello, world!' text should exist.")
+    func testShouldShowSwiftTestingLabel() throws {
+        XCTAssertTrue(page.swiftTestingLabel.exists, "The 'Hello Swift Testing' text should exist.")
     }
 }
